118e9d5bbf905936acf02111bdb4d00b0a945c69
Jesse Shawl <jesse@jesse.sh>
2024-03-18 21:32:46 -0500

    support urlencoded form data (#4)
    * support urlencoded form data
    
    * remove prettierrc

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 const validate = async (request) => {
-	const required = ['subject', 'to'];
+	const required = ["subject", "to"];
 	let data;
 	try {
-		data = (await request.json()) ?? {};
+		if (request.headers.get("content-type") === "application/json") {
+			data = (await request.json()) ?? {};
+		}
+
+		if (
+			request.headers.get("content-type") ===
+			"application/x-www-form-urlencoded"
+		) {
+			data = Object.fromEntries(
+				new URLSearchParams((await request.text()) ?? ""),
+			);
+		}
 	} catch (e) {
 		data = {};
 	}
 
-	data.errors = required.reduce((acc, el) => {
-		if (!data[el]) {
-			return [...acc, `missing required parameter '${el}'`];
-		}
-		return acc;
-	}, []);
+	data.errors = required.reduce(
+		(acc, el) =>
+			data[el] ? acc : [...acc, `missing required parameter '${el}'`],
+		[],
+	);
 
 	return data;
 };
@@ -20,7 +30,7 @@ const validate = async (request) => {
 export default {
 	async fetch(request, env, ctx) {
 		const { pathname } = new URL(request.url);
-		if (pathname === '/api/notify') {
+		if (pathname === "/api/notify") {
 			const { subject, to, errors } = await validate(request);
 			const body = {
 				subject,
@@ -30,13 +40,15 @@ export default {
 				...body,
 				...(errors.length && { errors }),
 			};
-			return new Response(JSON.stringify(response), { status: errors.length ? 400 : 200 });
+			return new Response(JSON.stringify(response), {
+				status: errors.length ? 400 : 200,
+			});
 		}
 		return new Response(
 			JSON.stringify(
 				{
-					message: 'Not Found',
-					documentation_url: 'https://www.cinotify.cc/docs',
+					message: "Not Found",
+					documentation_url: "https://www.cinotify.cc/docs",
 				},
 				null,
 				2,
