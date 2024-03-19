55b03c4b152625775cdf87facd65e223ee469b67
Jesse Shawl <jesse@jesse.sh>
2024-03-18 21:37:37 -0500

    format

--- src/index.js
+++ src/index.js
@@ -1,59 +1,50 @@
 const validate = async (request) => {
-	const required = ["subject", "to"];
-	let data;
-	try {
-		if (request.headers.get("content-type") === "application/json") {
-			data = (await request.json()) ?? {};
-		}
+  const required = ['subject', 'to'];
+  let data;
+  try {
+    if (request.headers.get('content-type') === 'application/json') {
+      data = (await request.json()) ?? {};
+    }
 
-		if (
-			request.headers.get("content-type") ===
-			"application/x-www-form-urlencoded"
-		) {
-			data = Object.fromEntries(
-				new URLSearchParams((await request.text()) ?? ""),
-			);
-		}
-	} catch (e) {
-		data = {};
-	}
+    if (request.headers.get('content-type') === 'application/x-www-form-urlencoded') {
+      data = Object.fromEntries(new URLSearchParams((await request.text()) ?? ''));
+    }
+  } catch (e) {
+    data = {};
+  }
 
-	data.errors = required.reduce(
-		(acc, el) =>
-			data[el] ? acc : [...acc, `missing required parameter '${el}'`],
-		[],
-	);
+  data.errors = required.reduce((acc, el) => (data[el] ? acc : [...acc, `missing required parameter '${el}'`]), []);
 
-	return data;
+  return data;
 };
 
 export default {
-	async fetch(request, env, ctx) {
-		const { pathname } = new URL(request.url);
-		if (pathname === "/api/notify") {
-			const { subject, to, errors } = await validate(request);
-			const body = {
-				subject,
-				to,
-			};
-			const response = {
-				...body,
-				...(errors.length && { errors }),
-			};
-			return new Response(JSON.stringify(response), {
-				status: errors.length ? 400 : 200,
-			});
-		}
-		return new Response(
-			JSON.stringify(
-				{
-					message: "Not Found",
-					documentation_url: "https://www.cinotify.cc/docs",
-				},
-				null,
-				2,
-			),
-			{ status: 404 },
-		);
-	},
+  async fetch(request, env, ctx) {
+    const { pathname } = new URL(request.url);
+    if (pathname === '/api/notify') {
+      const { subject, to, errors } = await validate(request);
+      const body = {
+        subject,
+        to,
+      };
+      const response = {
+        ...body,
+        ...(errors.length && { errors }),
+      };
+      return new Response(JSON.stringify(response), {
+        status: errors.length ? 400 : 200,
+      });
+    }
+    return new Response(
+      JSON.stringify(
+        {
+          message: 'Not Found',
+          documentation_url: 'https://www.cinotify.cc/docs',
+        },
+        null,
+        2,
+      ),
+      { status: 404 },
+    );
+  },
 };
