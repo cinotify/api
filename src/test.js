fc7536ff3155991238fe4a8d16f94c85d6c2e5fa
Jesse Shawl <jesse@jesse.sh>
2024-03-21 17:37:14 -0500

    add type input

--- src/test.js
+++ src/test.js
@@ -6,9 +6,10 @@ export const input = {
       filename: 'hello.txt',
     },
   ],
-  body: 'this is the body',
+  body: '<h1>an html body</h1>',
   subject: 'hello',
   to: 'ex@mple.com',
+  type: 'text/html'
 };
 
-export const inputUrlEncoded = `to=${input.to}&subject=${input.subject}&body=${input.body}&attachments[][type]=text/plain&attachments[][content]=aGVsbG8sIHdvcmxkIQ==&attachments[][filename]=hello.txt`;
+export const inputUrlEncoded = `to=${input.to}&subject=${input.subject}&body=${input.body}&attachments[][type]=text/plain&attachments[][content]=aGVsbG8sIHdvcmxkIQ==&attachments[][filename]=hello.txt&type=text/html`;
