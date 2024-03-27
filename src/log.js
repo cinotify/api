d68952fde33141497d109a832b36d7c4ff6e85af
Jesse Shawl <jesse@jesse.sh>
2024-03-27 07:02:50 -0500

    log log response

--- src/log.js
+++ src/log.js
@@ -1,5 +1,5 @@
 export const log = async ({ env, user_id, tags }) => {
-  await fetch('https://api.logsnag.com/v1/log', {
+  const response = await fetch('https://api.logsnag.com/v1/log', {
     headers: {
       'Content-Type': 'application/json',
       Authorization: `Bearer ${env.LOGSNAG_API_TOKEN}`,
@@ -14,4 +14,7 @@ export const log = async ({ env, user_id, tags }) => {
     }),
     method: 'POST',
   });
+  const data = await response.json();
+  // eslint-disable-next-line no-console
+  console.log(data);
 };
